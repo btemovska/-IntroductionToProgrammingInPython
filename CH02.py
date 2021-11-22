45e9bea773ef3a1b34946dd32f9dbe9309dd9e95
btemovska <btemovska@gmail.com>
2021-11-21 20:21:31 -0500

    CH02

---
+++ CH02.py
@@ -0,0 +1,26 @@
+#2.12 LAB
+userNum = int(input())
+userNumSquared = userNum * userNum   # Bug here; fix it when instructed
+
+print(userNumSquared)       # Output formatting issue here; fix it when instructed
+
+
+#2.13 LAB
+print('Enter x: ')
+x = int(input())
+
+# print(x) # Student mistakenly is echoing the input to output to match example
+print('x doubled is:', (2 * x))
+
+#2.15 LAB
+user_name = input()
+
+print('Hello', user_name, 'and welcome to CS Online!')
+
+#2.16 LAB
+first_name = input()
+generic_location = input()
+whole_number = input()
+plural_noun = input()
+
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
