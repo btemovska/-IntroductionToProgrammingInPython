fd74d0a81e1ac08a936f8a607ff20091a9b3f565
btemovska <btemovska@gmail.com>
2021-12-01 19:52:36 -0500

    Lab22.2

---
+++ Lab22.2.py
@@ -0,0 +1,13 @@
+string1 = input()
+string2 = input()
+
+string1_length = len(string1)
+string2_length = len(string2)
+
+# print(string1_length)
+# print(string2_length)
+
+if string1_length > string2_length:
+    print(string1)
+else:
+    print(string2)
