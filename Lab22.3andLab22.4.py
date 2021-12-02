9b2f6de9c20f6677b0c1a9e07973e3695b40fc1f
btemovska <btemovska@gmail.com>
2021-12-02 17:12:09 -0500

    Lab22.3 and Lab22.4

---
+++ Lab22.3andLab22.4.py
@@ -0,0 +1,23 @@
+#Lab 22.3
+num_one = int(input())
+num_two = int(input())
+
+if num_one > num_two:
+    print(num_one)
+else:
+    print(num_two)
+
+#Lab 22.4
+num_one = int(input())
+num_two = int(input())
+num_three = int(input())
+
+list = []
+list.append(num_one)
+list.append(num_two)
+list.append(num_three)
+
+#print(list)
+max_number = max(list)
+print(max_number)
+
