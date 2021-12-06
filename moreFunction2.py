610fdf4de8ac0ad1d3b3032b685fd5e89d30af76
btemovska <btemovska@gmail.com>
2021-12-06 18:57:59 -0500

    moreFunction2

---
+++ moreFunction2.py
@@ -0,0 +1,12 @@
+def swap_values(user_val1, user_val2):
+    return '{1} {0}'.format(user_val1, user_val2)
+
+num_1 = int(input())
+num_2 = int(input())
+
+print(swap_values(num_1, num_2))
+
+
+
+
+
