833f8335d12c1decb2d6d5dd2f5117581c27f4bf
btemovska <btemovska@gmail.com>
2021-12-07 18:53:20 -0500

    section7.17

---
+++ section7.17.py
@@ -0,0 +1,15 @@
+gas_const = 8.3144621
+
+def compute_gas_volume(pressure, temperature, moles):
+    gas_volume (moles * gas_const * temperature) / pressure
+    return gas_volume
+
+
+gas_pressure = float(input())
+gas_moles = float(input())
+gas_temperature = float(input())
+gas_volume = 0.0
+
+gas_volume = compute_gas_volume(gas_pressure, gas_temperature, gas_moles)
+print('Gas volume:', gas_volume, 'm^3')
+
