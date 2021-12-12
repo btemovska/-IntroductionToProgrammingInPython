637e438f2801c2ef8a478f0bc1f0f2c45f2e3733
btemovska <btemovska@gmail.com>
2021-12-11 20:11:38 -0500

    Lab 26.6

---
+++ Lab26.6.py
@@ -0,0 +1,13 @@
+def reverse_list(letters):
+    # Type your code here.
+    return letters[::-1]
+
+if __name__ == '__main__':
+    ch = ['a', 'b', 'c']
+    print(reverse_list(ch)) # Should print ['c', 'b', 'a']
+
+
+
+
+
+
