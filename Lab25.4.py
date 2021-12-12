0e9483ba76dfb61e996e2ddfd0d23f40ed406cdd
btemovska <btemovska@gmail.com>
2021-12-12 13:13:33 -0500

    Lab 25.4

---
+++ Lab25.4.py
@@ -0,0 +1,17 @@
+def remove_spaces(user_string):
+    # Type your code here.
+    return user_string.replace(" ", "")
+
+
+
+if __name__ == '__main__':
+    # Type your code here.
+    user_entered = input()
+
+    print(remove_spaces(user_entered))
+
+
+
+
+
+
