479a1f6e42bcab0dfd2e084af62082ddc9cc70bf
btemovska <btemovska@gmail.com>
2021-12-15 11:59:06 -0500

    Lab27.4

---
+++ Lab27.4.py
@@ -0,0 +1,27 @@
+class Student:
+    # Type your code here
+    def __init__(self):
+        self.name = 'Louie'
+        self.gpa = 1.0
+
+    def set_name(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
+
+    def set_gpa(self, gpa):
+        self.gpa = gpa
+    def get_gpa(self):
+        return self.gpa
+
+if __name__ == "__main__":
+    initial_student = Student()
+    print(initial_student.get_name(),'/', initial_student.get_gpa())
+
+    initial_student.set_name('Felix')
+    initial_student.set_gpa(3.7)
+    print(initial_student.get_name(), '/', initial_student.get_gpa())
+
+
+
+
