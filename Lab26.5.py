f030efbf146c43ea322fc26d737f58a8ff810bbb
btemovska <btemovska@gmail.com>
2021-12-15 19:29:45 -0500

    Lab26.5

---
+++ Lab26.5.py
@@ -0,0 +1,12 @@
+def calc_average(nums):
+    # Type your code here.
+    x = sum(nums) / len(nums)
+    return x
+
+if __name__ == '__main__':
+    nums = [1, 2, 3, 4, 5]
+    print(calc_average(nums))   # calc_average() should return 3.0
+
+
+
+
