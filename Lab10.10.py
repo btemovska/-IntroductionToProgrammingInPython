4079e75015a35dfeb01d414e33eb713a4953c818
btemovska <btemovska@gmail.com>
2021-12-17 19:35:50 -0500

    Lab10.10

---
+++ Lab10.10.py
@@ -0,0 +1,47 @@
+#not my code followed the instructor
+# Define custom exception
+class StudentInfoError(Exception):
+    def __init__(self, message):
+        self.message = message  # Initialize the exception message
+
+
+def find_ID(name, info):
+    # Type your code here.
+    if name in info:
+        return info[name]
+    raise StudentInfoError('Student ID not found for {}'.format(name))
+
+
+def find_name(ID, info):
+    # Type your code here.
+    for name in info:
+        if ID == info[name]:
+            return name
+    raise StudentInfoError('Student name not found for {}'.format(ID))
+
+
+if __name__ == '__main__':
+    # Dictionary of student names and IDs
+    student_info = {
+        'Reagan' : 'rebradshaw835',
+        'Ryley' : 'rbarber894',
+        'Peyton' : 'pstott885',
+        'Tyrese' : 'tmayo945',
+        'Caius' : 'ccharlton329'
+    }
+
+    userChoice = input()    # Read search option from user. 0: find_ID(), 1: find_name()
+
+    # FIXME: find_ID() and find_name() may throw an Exception.
+    #        Insert a try/except statement to catch the exception and output any exception message.
+
+    try:
+        if userChoice == "0":
+            name = input()
+            result = find_ID(name, student_info)
+        else:
+            ID = input()
+            result = find_name(ID, student_info)
+        print(result)
+    except StudentInfoError as excpt:
+        print(excpt)
