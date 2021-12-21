2782cc8c2399acdaa294eeaf4ec0842dbcd4744c
btemovska <btemovska@gmail.com>
2021-12-21 10:17:54 -0500

    Lab10.8

---
+++ Lab10.8.py
@@ -0,0 +1,26 @@
+# Split input into 2 parts: name and age
+parts = input().split()
+name = parts[0]
+while name != '-1':
+    # FIXME: The following line will throw ValueError exception.
+    #        Insert try/except blocks to catch the exception.
+
+    try:
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+
+        parts = input().split()
+        name = parts[0]
+
+    # Get next line
+
+    except ValueError:
+        age = 0
+        print('{} {}'.format(name, age))
+        parts = input().split()
+        name = parts[0]
+
+
+
+
+
