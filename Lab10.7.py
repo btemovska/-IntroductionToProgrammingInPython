06e4a3ff5eebfc2bdb160ba3e5bfd8e582dc61f9
btemovska <btemovska@gmail.com>
2021-12-21 09:54:34 -0500

    Lab10.7

--- Lab10.7.py
+++ Lab10.7.py
@@ -1,25 +1,23 @@
 def get_age():
     age = int(input())
-    # TODO: Raise exception for invalid ages
-    if age >= 18 and age <= 75:
+    if age >=18 and age <=75:
         return age
-    raise ValueError('Invalid age.\nCould not calculate heart rate info.')
-
-    # return age
-
+    raise ValueError("Invalid age.\nCould not calculate heart rate info.")
+    # TODO: Raise exception for invalid ages
+    #return age
 
 # TODO: Complete fat_burning_heart_rate() function
 def fat_burning_heart_rate(age):
-    heart_rate = (220 - age) * 0.70
-    return 'Fat burning heart rate for a {} year-old: {} bpm'.format(age, heart_rate)
-
+    x = 220 - age
+    heart_rate = x * 0.70
+    return heart_rate
 
 if __name__ == "__main__":
     # TODO: Modify to call get_age() and fat_burning_heart_rate()
     #       and handle the exception
     try:
         age = get_age()
-        print(fat_burning_heart_rate(age))
+        heart_rate = fat_burning_heart_rate(age)
+        print('Fat burning heart rate for a {} year-old: {} bpm'.format(age, heart_rate))
     except ValueError as x:
         print(x)
-
