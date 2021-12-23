926a57f13f2f797b3d6d137d7fa3e1157a8120bd
btemovska <btemovska@gmail.com>
2021-12-22 19:06:25 -0500

    Lab9.19

--- Lab9.19.py
+++ Lab9.19.py
@@ -1,36 +1,23 @@
-word_pairs = 'Mo 391-0993 Rachel 019-1265 Ruby 010-8712 Steve 312-3318 Maria 871-0091'
-name_chosen = 'Rachel'
-
-word_pairs = word_pairs.split(' ')
-#print(word_pairs) #creates list
-
-#print(len(word_pairs))
-key_and_value_pairs = int(len(word_pairs) / 2)
-#print(key_and_value_pairs)
-
 my_dict = {}
+user_entered = input() #Joe 123-5432 Linda 983-4123 Frank 867-5309
+user_entered = user_entered.split()
 
-if key_and_value_pairs == 1:
-    my_dict[word_pairs[0]] = word_pairs[1]
-elif key_and_value_pairs == 2:
-    my_dict[word_pairs[0]] = word_pairs[1]
-    my_dict[word_pairs[2]] = word_pairs[3]
-elif key_and_value_pairs == 3:
-    my_dict[word_pairs[0]] = word_pairs[1]
-    my_dict[word_pairs[2]] = word_pairs[3]
-    my_dict[word_pairs[4]] = word_pairs[5]
-elif key_and_value_pairs == 4:
-    my_dict[word_pairs[0]] = word_pairs[1]
-    my_dict[word_pairs[2]] = word_pairs[3]
-    my_dict[word_pairs[4]] = word_pairs[5]
-    my_dict[word_pairs[6]] = word_pairs[7]
-elif key_and_value_pairs == 5:
-    my_dict[word_pairs[0]] = word_pairs[1]
-    my_dict[word_pairs[2]] = word_pairs[3]
-    my_dict[word_pairs[4]] = word_pairs[5]
-    my_dict[word_pairs[6]] = word_pairs[7]
-    my_dict[word_pairs[8]] = word_pairs[9]
+names = [] #creating list just for names
+numbers = [] #creating list just for numbers
 
-#print(my_dict)
+for x in user_entered:
+    if x.isalpha():
+        names.append(x)
+    else:
+        numbers.append(x)
 
-print(my_dict.get(name_chosen))
+my_dict = dict(zip(names, numbers)) #this creates the dict
+
+number_to_get = input() #ask for name in the dict (Linda)
+phone_number = ''
+
+if number_to_get in my_dict.keys():
+    phone_number = my_dict.get(number_to_get)
+
+print(phone_number) #983-4123
+
