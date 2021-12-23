b00adfb2ccdb69aebee32e5d04ac3adb4b033b41
btemovska <btemovska@gmail.com>
2021-12-22 20:30:58 -0500

    Lab9.21

--- Lab9.21.py
+++ Lab9.21.py
@@ -1,5 +1,7 @@
-#TASK 1
+# TASK 1
 print("Task 1")
+
+
 # Complete the function to return the first two items in the given list
 def getFirstTwo(mylist):
     # Student code goes here
@@ -8,55 +10,67 @@ def getFirstTwo(mylist):
     list.append(mylist[1])
     return list
 
+
 # expected output: [8, 3]
-print(getFirstTwo([8,3,5,2,10]))
+print(getFirstTwo([8, 3, 5, 2, 10]))
 
 # expected output: [15, 2]
-print(getFirstTwo([15,2,10,12]))
+print(getFirstTwo([15, 2, 10, 12]))
 
-#TASK 2
+# TASK 2
 print("Task 2")
+
+
 # Complete the function to return the last two items in the given list
 def getLastTwo(mylist):
     # Student code goes here
     return mylist[-2:]
 
+
 # expected output: [2, 10]
-print(getLastTwo([8,3,5,2,10]))
+print(getLastTwo([8, 3, 5, 2, 10]))
 
 # expected output: [9, 12]
-print(getLastTwo([15,2,9,12]))
+print(getLastTwo([15, 2, 9, 12]))
 
-#TASK 3
+# TASK 3
 print("Task 3")
+
+
 # Complete the function to add num1 to the end of the given list
 def addToEnd(mylist, num1):
     # Student code goes here
     mylist.append(num1)
     return mylist
 
+
 # expected output: [4, 5, 6, 7]
-print(addToEnd([4,5,6], 7))
+print(addToEnd([4, 5, 6], 7))
 
 # expected output: [9, 8, 7, 6]
-print(addToEnd([9,8,7], 6))
+print(addToEnd([9, 8, 7], 6))
 
-#TASK 4
+# TASK 4
 print("Task 4")
+
+
 # Complete the function to add num1 to the front of the given list
 def addToFront(mylist, num1):
     # Student code goes here
     mylist.insert(0, num1)
     return mylist
 
+
 # expected output: [3, 4, 5, 6]
-print(addToFront([4,5,6], 3))
+print(addToFront([4, 5, 6], 3))
 
 # expected output: [10, 9, 8, 7]
-print(addToFront([9,8,7], 10))
+print(addToFront([9, 8, 7], 10))
 
-#TASK 5
+# TASK 5
 print("Task 5")
+
+
 # Complete the function to return a new list containing
 # the first two and last two items in the given list
 def getFirstTwoAndLastTwo(mylist):
@@ -64,31 +78,37 @@ def getFirstTwoAndLastTwo(mylist):
     my_new_list = []
     first_two = mylist[:2]
     last_two = mylist[-2:]
-    my_new_list = first_two + last_two #unpacks list from nested list
+    my_new_list = first_two + last_two  # unpacks list from nested list
     return my_new_list
 
+
 # expected output: [8, 3, 19, 1]
-print(getFirstTwoAndLastTwo([8,3,7,15,2,10,19,1]))
+print(getFirstTwoAndLastTwo([8, 3, 7, 15, 2, 10, 19, 1]))
 
 # expected output: [7, 15, 3, 5]
-print(getFirstTwoAndLastTwo([7,15,2,10,19,1,3,5]))
+print(getFirstTwoAndLastTwo([7, 15, 2, 10, 19, 1, 3, 5]))
 
-#TASK 6
+# TASK 6
 print("Task 6")
+
+
 # Complete the function to remove the first item in the given list
 def removeFirst(mylist):
     # Student code goes here
     mylist.remove(mylist[0])
     return mylist
 
+
 # expected output: [7, 8, 9]
-print(removeFirst([6,7,8,9]))
+print(removeFirst([6, 7, 8, 9]))
 
 # expected output: [2, 3, 4]
-print(removeFirst([1,2,3,4]))
+print(removeFirst([1, 2, 3, 4]))
 
-#TASK 7
+# TASK 7
 print("Task 7")
+
+
 # Complete the function to remove the third item in the given list
 def removeThird(mylist):
     # Student code goes here
@@ -96,14 +116,17 @@ def removeThird(mylist):
     mylist.remove(third_item)
     return mylist
 
+
 # expected output: [6, 7, 9]
-print(removeThird([6,7,8,9]))
+print(removeThird([6, 7, 8, 9]))
 
 # expected output: [1, 2, 4]
-print(removeThird([1,2,3,4]))
+print(removeThird([1, 2, 3, 4]))
 
-#TASK 8
+# TASK 8
 print("Task 8")
+
+
 # Complete the function to order the values in the list
 # if ascending is true then order lowest to highest
 # if ascending is false then order highest to lowest
@@ -116,14 +139,17 @@ def sortList(mylist, ascending):
         x = y[::-1]
     return x
 
+
 # expected output: [4, 12, 19, 33]
-print(sortList([19,4,33,12], True))
+print(sortList([19, 4, 33, 12], True))
 
 # expected output: [33, 19, 12, 4]
-print(sortList([19,4,33,12], False))
+print(sortList([19, 4, 33, 12], False))
 
-#TASK 9
+# TASK 9
 print("Task 9")
+
+
 # Complete the function to return a dictionary using
 # list1 as the keys and list2 as the values
 def createDict(list1, list2):
@@ -132,9 +158,71 @@ def createDict(list1, list2):
     my_dict = dict(zip(list1, list2))
     return my_dict
 
+
 # expected output: {'tomato': 'red', 'banana': 'yellow', 'lime': 'green'}
-print(createDict(['tomato', 'banana', 'lime'], ['red','yellow','green']))
+print(createDict(['tomato', 'banana', 'lime'], ['red', 'yellow', 'green']))
 
 # expected output: {'Brazil': 'Brasilia', 'Ireland': 'Dublin', 'Indonesia': 'Jakarta'}
-print(createDict(['Brazil', 'Ireland', 'Indonesia'], ['Brasilia','Dublin','Jakarta']))
+print(createDict(['Brazil', 'Ireland', 'Indonesia'], ['Brasilia', 'Dublin', 'Jakarta']))
 
+# TASK 10
+print('Task 10')
+
+
+# Complete the function to return a dictionary value
+# if it exists or return Not Found if it doesn't exist
+def findDictItem(mydict, key):
+    # Student code goes here
+    if key in mydict.keys():
+        return mydict.get(key)
+    else:
+        x = 'Not Found'
+        return x
+
+
+# expected output: yellow
+print(findDictItem({'tomato': 'red', 'banana': 'yellow', 'lime': 'green'}, 'banana'))
+
+# expected output: Not Found
+print(findDictItem({'Brazil': 'Brasilia', 'Ireland': 'Dublin', 'Indonesia': 'Jakarta'}, 'Cameroon'))
+
+# TASK 11
+print('Task 11')
+
+
+# Complete the function to remove a dictionary item if it exists
+def removeDictItem(mydict, key):
+    # Student code goes here
+    if key in mydict.keys():
+        del mydict[key]
+    return mydict
+
+
+# expected output: {'tomato': 'red', 'lime': 'green'}
+print(removeDictItem({'tomato': 'red', 'banana': 'yellow', 'lime': 'green'}, 'banana'))
+
+# expected output: {'Brazil': 'Brasilia', 'Ireland': 'Dublin', 'Indonesia': 'Jakarta'}
+print(removeDictItem({'Brazil': 'Brasilia', 'Ireland': 'Dublin', 'Indonesia': 'Jakarta'}, 'Cameroon'))
+
+# TASK 12
+print('Task 12')
+
+
+# Complete the function to print every key and value
+def my_dict(mydict):
+    # Student code goes here
+    for x, y in mydict.items():
+        print('{}={}'.format(x, y))
+
+
+# expected output:
+#        tomato=red
+#        banana=yellow
+#        lime=green
+my_dict({'tomato': 'red', 'banana': 'yellow', 'lime': 'green'})
+
+# expected output:
+#        Brazil=Brasilia
+#        Ireland=Dublin
+#        Indonesia=Jakarta
+my_dict({'Brazil': 'Brasilia', 'Ireland': 'Dublin', 'Indonesia': 'Jakarta'})
