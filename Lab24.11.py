c1f2c503d30d4fc4344451efc5de9a863eed8cd5
btemovska <btemovska@gmail.com>
2021-12-24 14:56:32 -0500

    Lab 24.11

--- Lab24.11.py
+++ Lab24.11.py
@@ -1,35 +1,19 @@
-#this needs re-done as the test are failing
-
 def largest_number(num1, num2, num3):
     # Type your code here.
-    largest_list = []
-    largest_list.append(num1)
-    largest_list.append(num2)
-    largest_list.append(num3)
-    sorted_list = sorted(largest_list)
-    num = sorted_list[-1]
-    print('largest: ', end='')
-    print(num)
-
+    largest_number = max(num1, num2, num3)
+    return largest_number
 
 def smallest_number(num1, num2, num3):
-    #Type your code here.
-    smallest_list = []
-    smallest_list.append(num1)
-    smallest_list.append(num2)
-    smallest_list.append(num3)
-    sorted_list = sorted(smallest_list)
-    num_a = sorted_list[0]
-    print('smallest: ', end='')
-    print(num_a)
+    # Type your code here.
+    smallest_number = min(num1, num2, num3)
+    return smallest_number
 
 
 if __name__ == '__main__':
     # Type your code here.
-    num1 = int(input())
-    num2 = int(input())
-    num3 = int(input())
+    int1 = int(input())
+    int2 = int(input())
+    int3 = int(input())
 
-    largest_number(num1, num2, num3)
-    smallest_number(num1, num2, num3)
-
+    print('largest: {}'.format(largest_number(int1, int2, int3)))
+    print('smallest: {}'.format(smallest_number(int1, int2, int3)))
