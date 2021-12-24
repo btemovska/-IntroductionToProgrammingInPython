09d7d0d9dae63a727a5f8102add9d13715bdc7ad
btemovska <btemovska@gmail.com>
2021-12-23 19:34:57 -0500

    Lab25.6

---
+++ Lab25.6.py
@@ -0,0 +1,86 @@
+def get_month_as_int(monthString):
+
+    if monthString == 'January':
+        month_int = 1
+    elif monthString == 'February':
+        month_int = 2
+    elif monthString == 'March':
+        month_int = 3
+    elif monthString == 'April':
+        month_int = 4
+    elif monthString == 'May':
+        month_int = 5
+    elif monthString == 'June':
+        month_int = 6
+    elif monthString == 'July':
+        month_int = 7
+    elif monthString == 'August':
+        month_int = 8
+    elif monthString == 'September':
+        month_int = 9
+    elif monthString == 'October':
+        month_int = 10
+    elif monthString == 'November':
+        month_int = 11
+    elif monthString == 'December':
+        month_int = 12
+    else:
+        month_int = 0
+
+    return month_int
+
+
+user_string = input()
+my_list = []
+
+while user_string != '-1':
+    my_list.append(user_string)
+    user_string = input()
+
+
+my_new_list = []
+for x in my_list:
+    if x.__contains__(','):
+        my_new_list.append(x)
+
+list1 = []
+year_list = []
+month_list = []
+day_list = []
+
+for x in my_new_list:
+    first_space = x.find(' ')
+    month = x[0:first_space]
+    month_list.append(month)
+    year = x[-4:]
+    year_list.append(year)
+    find_comma = x.find(',')
+    date = x[first_space+1:find_comma]
+    day_list.append(date)
+
+month_list_numb = []
+for x in month_list:
+    month_numb = get_month_as_int(x)
+    month_list_numb.append(month_numb)
+
+if len(month_list_numb) == 5:
+    print('{}/{}/{}'.format(month_list_numb[0], day_list[0], year_list[0]))
+    print('{}/{}/{}'.format(month_list_numb[1], day_list[1], year_list[1]))
+    print('{}/{}/{}'.format(month_list_numb[2], day_list[2], year_list[2]))
+    print('{}/{}/{}'.format(month_list_numb[3], day_list[3], year_list[3]))
+    print('{}/{}/{}'.format(month_list_numb[4], day_list[4], year_list[4]))
+if len(month_list_numb) == 4:
+    print('{}/{}/{}'.format(month_list_numb[0], day_list[0], year_list[0]))
+    print('{}/{}/{}'.format(month_list_numb[1], day_list[1], year_list[1]))
+    print('{}/{}/{}'.format(month_list_numb[2], day_list[2], year_list[2]))
+    print('{}/{}/{}'.format(month_list_numb[3], day_list[3], year_list[3]))
+if len(month_list_numb) == 3:
+    print('{}/{}/{}'.format(month_list_numb[0], day_list[0], year_list[0]))
+    print('{}/{}/{}'.format(month_list_numb[1], day_list[1], year_list[1]))
+    print('{}/{}/{}'.format(month_list_numb[2], day_list[2], year_list[2]))
+if len(month_list_numb) == 2:
+    print('{}/{}/{}'.format(month_list_numb[0], day_list[0], year_list[0]))
+    print('{}/{}/{}'.format(month_list_numb[1], day_list[1], year_list[1]))
+if len(month_list_numb) == 1:
+    print('{}/{}/{}'.format(month_list_numb[0], day_list[0], year_list[0]))
+
