305125656b052364897c7b5f1aa4ff88d10889f1
btemovska <btemovska@gmail.com>
2022-01-16 13:30:14 -0500

    Lab8.8

--- Lab8.8.py
+++ Lab8.8.py
@@ -1,4 +1,4 @@
-
+#Lab 8.8
 string_entered = input()
 
 while string_entered != 'quit 0':
@@ -10,4 +10,16 @@ while string_entered != 'quit 0':
     print('Eating {} {} a day keeps the doctor away.'.format(integer, phrase))
     string_entered = input()
 
+#much better way
+user_entered = input()
 
+space_index = user_entered.find(' ') #6
+user_word = user_entered[0:space_index]
+user_digit = user_entered[space_index+1:]
+
+while user_word != 'quit':
+    print(f'Eating {user_digit} {user_word} a day keeps the doctor away.')
+    user_entered = input()
+    space_index = user_entered.find(' ')
+    user_word = user_entered[0:space_index]
+    user_digit = user_entered[space_index+1:]
