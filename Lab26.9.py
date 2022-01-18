6e146568c08a66a1f50b9a20daa1d5fd7177d3d8
btemovska <btemovska@gmail.com>
2022-01-17 19:33:42 -0500

    Lab26.9

--- Lab26.9.py
+++ Lab26.9.py
@@ -21,3 +21,29 @@ if __name__ == '__main__':
 
     get_user_values(nums)
     output_ints_less_than_or_equal_to_threshold(nums, threshold)
+
+
+#BETTER
+# Define your get_user_values function here
+def get_user_values(nums):
+    user_num = int(input())
+
+    while user_num != -1:
+        nums.append(user_num)
+        user_num = int(input())
+
+    return nums
+
+# Define your output_ints_less_than_or_equal_to_threshold function here
+def output_ints_less_than_or_equal_to_threshold(nums, threshold):
+    for x in nums:
+        if x <= threshold:
+            print(x)
+
+
+if __name__ == '__main__':
+    threshold = int(input())
+    nums = []
+
+    get_user_values(nums)
+    output_ints_less_than_or_equal_to_threshold(nums, threshold)
