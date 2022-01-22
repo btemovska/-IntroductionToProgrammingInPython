bf17b51ab6c78d96a87a4d2a646608dd24b10447
btemovska <btemovska@gmail.com>
2022-01-22 14:53:12 -0500

    Try_Except

---
+++ Try_Except.py
@@ -0,0 +1,14 @@
+#example 1
+try:
+    first_number = int(input('Enter first number:\n'))
+    second_number = int(input('Enter second number:\n'))
+    result = first_number / second_number
+    print('Result: {:.0f}'.format(result))
+
+except ZeroDivisionError:
+    print('Can\'t devide by 0')
+except ValueError:
+    print('Invalid number')
+
+#example 2
+
