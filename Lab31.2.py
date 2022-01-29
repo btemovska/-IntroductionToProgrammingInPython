a7d52066fc275e702a289be57be376d9f961f861
btemovska <btemovska@gmail.com>
2022-01-29 11:30:56 -0500

    Lab31.2

---
+++ Lab31.2.py
@@ -0,0 +1,8 @@
+"""Write a program whose inputs are three integers,
+and whose output is the smallest of the three values.
+"""
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+print(min(num1, num2, num3))
