1eff8c65d4e4aceb7851545b23535735378fc2ff
btemovska <btemovska@gmail.com>
2022-01-29 11:31:47 -0500

    Lab31.1

--- Lab31.1.py
+++ Lab31.1.py
@@ -1,3 +1,6 @@
+"""Given four values representing counts of quarters, dimes, nickels and pennies,
+output the total amount as dollars and cents."""
+
 quarters = int(input())
 dimes = int(input())
 nickles = int(input())
