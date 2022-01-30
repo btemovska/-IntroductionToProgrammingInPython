aecbfbd2a46c592b9daad439e1635968e5cdb2e4
btemovska <btemovska@gmail.com>
2022-01-30 10:57:34 -0500

    Lab31.10

---
+++ Lab31.10.py
@@ -0,0 +1,12 @@
+"""Write a program that prints a formatted "No parking" sign as shown below.
+Note the first line has two leading spaces. For ALL labs, end with newline
+(unless otherwise stated).
+
+"""
+
+''' Type your code here. '''
+
+print('  NO PARKING')
+print('2:00 - 6:00 a.m.')
+
+
