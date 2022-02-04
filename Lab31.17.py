a6f4ee162f4c709bdf4a1000100344e718c230fc
btemovska <btemovska@gmail.com>
2022-02-03 19:14:14 -0500

    Lab31.17

---
+++ Lab31.17.py
@@ -0,0 +1,31 @@
+"""Write a program to calculate the total price for car wash services.
+A base car wash is $10. A dictionary with each additional service and
+the corresponding cost has been provided. Two additional services can be selected.
+A '-' signifies an additional service was not selected. Output all selected services,
+according to the input order, along with the corresponding costs and then the
+total price for all car wash services.
+"""
+
+
+services = { 'Air freshener' : 1 , 'Rain repellent': 2, 'Tire shine' : 2, 'Wax' : 3, 'Vacuum' : 5 }
+base_wash = 10
+total = 0
+
+service_choice1 = input()
+service_choice2 = input()
+
+''' Type your code here '''
+list1 = []
+list1.append(service_choice1)
+list1.append(service_choice2)
+
+list2 = []
+
+print('ZyCar Wash')
+print('Base car wash -- ${}'.format(base_wash))
+for x in list1:
+    if x in services.keys():
+        print('{} -- ${}'.format(x, services.get(x)))
+        list2.append(services.get(x))
+print('----')
+print('Total price: ${}'.format(base_wash + sum(list2)))
