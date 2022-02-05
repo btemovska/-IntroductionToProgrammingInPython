9734d66c09379e6cb603a5d2a8268eccced0f12b
btemovska <btemovska@gmail.com>
2022-02-05 12:34:13 -0500

    Lab24.8

--- Lab24.8.py
+++ Lab24.8.py
@@ -1,20 +1,34 @@
 # Define your function here
-my_list = []
+# my_list = []
+# def count_evens(num1, num2, num3, num4):
+#     my_list.append(num1)
+#     my_list.append(num2)
+#     my_list.append(num3)
+#     my_list.append(num4)
+#
+#     new_list = []
+#     for x in my_list:
+#         if x % 2 == 0:
+#             #print('even')
+#             new_list.append(x)
+#         else:
+#             pass
+#
+#     return len(new_list)
+
 def count_evens(num1, num2, num3, num4):
+    my_list = []
     my_list.append(num1)
     my_list.append(num2)
     my_list.append(num3)
     my_list.append(num4)
+    count = 0
 
-    new_list = []
     for x in my_list:
         if x % 2 == 0:
-            #print('even')
-            new_list.append(x)
-        else:
-            pass
+            count += 1
 
-    return len(new_list)
+    return count
 
 
 if __name__ == '__main__':
