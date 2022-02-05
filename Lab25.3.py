fb7a2b8c605f2140a000e592a092ebe301078d72
btemovska <btemovska@gmail.com>
2022-02-05 13:25:23 -0500

    Lab25.3

--- Lab25.3.py
+++ Lab25.3.py
@@ -17,4 +17,22 @@ if __name__ == '__main__':
     print(check_character('happy birthday!', 14))
 
 
+#OR
+def check_character(word, index):
+    character = word[index]
+    if character.isalpha():
+        return f'Character \'{character}\' is a letter'
+    elif character.isspace():
+        return f'Character \'{character}\' is a white space'
+    elif character.isdigit():
+        return f'Character \'{character}\' is a digit'
+    else:
+        return f'Character \'{character}\' is unknown'
+
+if __name__ == '__main__':
+    print(check_character('happy birthday', 2))
+    print(check_character('happy birthday', 5))
+    print(check_character('happy birthday 2 you', 15))
+    print(check_character('happy birthday!', 14))
+
 
