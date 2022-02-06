8b0eb8706f122778ce44a2ab25d998e7bd607f5f
btemovska <btemovska@gmail.com>
2022-02-05 20:05:14 -0500

    Lab29.1

--- Lab29.1.py
+++ Lab29.1.py
@@ -19,4 +19,22 @@ for rowNum in range(len(available)):
         print('{} ({}) -- {}'.format(name[rowNum], category[rowNum], description[rowNum]))
 
 
+# my code finally
+# Type your code here
+file_name = input()
 
+with open(file_name, 'r') as f:
+    foods_list = f.readlines()
+
+    for x in foods_list:
+        if x.__contains__('Available'):
+            #print(x.strip())
+            first_tab = x.find('\t')
+            category = x[:first_tab]
+            second_tab = (x[first_tab + 1 :].find('\t')) + first_tab
+            item = x[first_tab + 1 : second_tab + 1]
+            available_index = x.find('Available')
+            desc = x[second_tab +2 : available_index - 1]
+
+            print(f'{item} ({category}) -- {desc}')
+       
